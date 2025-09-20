08938fe4d7117e9ad5d02bf2ea6a471ba3ed5a30
VT341680@fi365.ort.edu.uy <VT341680@fi365.ort.edu.uy>
2025-09-20 19:35:56 -0300

    validateBody

---
+++ api/v1/middlewares/validate-body.middleware.js
@@ -0,0 +1,7 @@
+export const validateBody = (schema) => (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.message, details: error.details });
+    }
+    next();
+}
