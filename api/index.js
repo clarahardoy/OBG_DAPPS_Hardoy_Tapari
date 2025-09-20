bdf79873bba5af0faca87b4fb9bb34b82fbff663
Clara Hardoy <clarahardoy@icloud.com>
2025-09-20 18:53:35 -0300

    conexion base de datos

--- api/index.js
+++ api/index.js
@@ -2,14 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cors from 'cors';
-import { connectDB } from "./api/v1/config/db";
+import { connectDB } from "./v1/config/db.js";
 
-app.use(cors());
 connectDB();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
