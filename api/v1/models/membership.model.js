2169407256019631746637216df2b86209db3ed4
Clara Hardoy <clarahardoy@icloud.com>
2025-09-21 10:41:45 -0300

    sacar id del model

--- api/v1/models/membership.model.js
+++ api/v1/models/membership.model.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const membershipSchema = new Schema({
-    id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     bookMax: { type: Number, required: true },
 });
--- api/v1/models/shelf.model.js
+++ api/v1/models/shelf.model.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const shelfSchema = new Schema({
-    id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     books: { type: Array, required: true },
 });
