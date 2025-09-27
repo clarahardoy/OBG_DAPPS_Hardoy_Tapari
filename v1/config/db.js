3773461fd131a0a152d12efd3cb66fde95bee70f
VT341680@fi365.ort.edu.uy <VT341680@fi365.ort.edu.uy>
2025-09-27 16:19:50 -0300

    Mongo Atlas

--- v1/config/db.js
+++ v1/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.NODE_ENV === 'development'
+            ? process.env.MONGO_URI_DEV : process.env.MONGO_URI);
         console.log("Base de datos conectada");
     } catch (error) {
         console.error("Error al conectar a la base de datos", error);
