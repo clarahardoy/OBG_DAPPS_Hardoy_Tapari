13b0778116df85678954691b6a0a08cc13bf1f99
Clara Hardoy <clarahardoy@icloud.com>
2025-10-04 11:13:56 -0300

    fix user stats

--- v1/controllers/stats.controller.js
+++ v1/controllers/stats.controller.js
@@ -1,5 +1,5 @@
 import User from '../models/user.model.js';
-import { getUserReadingStats } from '../services/reading.service.js';
+import { StatsService } from '../services/stats.service.js';
 
 export const StatsController = {
     getMyReadingStats: async (req, res) => {
@@ -12,7 +12,7 @@ export const StatsController = {
                 return res.status(404).json({ message: 'Usuario no encontrado' });
             }
     
-            const stats = await getUserReadingStats(user._id, { year, month });
+            const stats = await StatsService.getUserReadingStats(user._id, { year, month });
             res.status(200).json({ message: 'Estadísticas obtenidas con éxito', stats });
         } catch (error) {
             res.status(error.status || 500).json({ message: 'Error al obtener estadísticas', error: error.message });
