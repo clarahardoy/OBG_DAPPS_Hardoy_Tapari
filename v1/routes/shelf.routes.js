7671913c69da50d3bff6af1f20c1e4d169068905
VT341680@fi365.ort.edu.uy <VT341680@fi365.ort.edu.uy>
2025-10-06 16:38:55 -0300

    Validators restantes

--- v1/routes/shelf.routes.js
+++ v1/routes/shelf.routes.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import { ShelfController } from '../controllers/shelf.controller.js';
 import { validateObjectIdMiddleware } from '../middlewares/validate-object-id.middleware.js';
+import { validateBodyMiddleware } from '../middlewares/validate-body.middleware.js';
+import { addShelfSchema, updateShelfSchema } from '../validators/shelf.validator.js';
 
 const SHELF_ROUTES = express.Router({ mergeParams: true });
 
 SHELF_ROUTES.get('/my-shelves', ShelfController.getUserShelves);
-SHELF_ROUTES.post('/', ShelfController.createShelf);
-SHELF_ROUTES.patch('/:id', validateObjectIdMiddleware, ShelfController.updateShelf);
+
+SHELF_ROUTES.post('/',
+    validateBodyMiddleware(addShelfSchema),
+    ShelfController.createShelf);
+
+SHELF_ROUTES.patch('/:id',
+    validateBodyMiddleware(updateShelfSchema),
+    validateObjectIdMiddleware,
+    ShelfController.updateShelf);
+
 SHELF_ROUTES.delete('/:id', validateObjectIdMiddleware, ShelfController.deleteShelf);
 
 // readings de una Shelf
--- v1/routes/user.routes.js
+++ v1/routes/user.routes.js
@@ -2,13 +2,15 @@
 import express from 'express';
 import { UserController } from '../controllers/user.controller.js';
 import { validateObjectIdMiddleware } from '../middlewares/validate-object-id.middleware.js';
+import { validateBodyMiddleware } from '../middlewares/validate-body.middleware.js';
+import { addUserSchema, updateUserSchema } from '../validators/user.validator.js';
 
 const USER_ROUTES = express.Router({ mergeParams: true });
 
 USER_ROUTES.get('/', UserController.getUsers);
 USER_ROUTES.get('/:id', validateObjectIdMiddleware, UserController.getUserById);
-USER_ROUTES.post('/', UserController.createUser);
-USER_ROUTES.put('/:id', validateObjectIdMiddleware, UserController.updateUser);
+USER_ROUTES.post('/', validateBodyMiddleware(addUserSchema), UserController.createUser);
+USER_ROUTES.put('/:id', validateBodyMiddleware(updateUserSchema), validateObjectIdMiddleware, UserController.updateUser);
 USER_ROUTES.delete('/:id', validateObjectIdMiddleware, UserController.deleteUser);
 
 export default USER_ROUTES;
