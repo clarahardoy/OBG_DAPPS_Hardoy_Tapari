bebb4dff4388ee277a053f182ed185e3efb90229
Clara Hardoy <clarahardoy@icloud.com>
2025-11-06 09:16:57 -0300

    logs

--- v1/config/db.js
+++ v1/config/db.js
@@ -1,11 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.NODE_ENV === 'development'
-            ? process.env.MONGO_URI_DEV : process.env.MONGO_URI);
-        console.log("Base de datos conectada");
-    } catch (error) {
-        console.error("Error al conectar a la base de datos", error);
-    }
+	console.log('🔍 NODE_ENV:', process.env.NODE_ENV);
+	console.log('🔍 Usando URI de', isDev ? 'DEV' : 'PROD');
+	console.log('🔍 URI definida?', !!uri);
+	try {
+		await mongoose.connect(
+			process.env.NODE_ENV === 'development'
+				? process.env.MONGO_URI_DEV
+				: process.env.MONGO_URI
+		);
+		console.log('Base de datos conectada');
+	} catch (error) {
+		console.error('Error al conectar a la base de datos', error);
+	}
 };
